964485cf2126f93b7cd0d8765cdb4630c9ea6f4f
卜乐宇 <isbobo0701@gmail.com>
2025-02-20 20:34:53 +0800

    登陆界面代码

---
+++ 机房预约管理系统/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int select = 0;
+
+	while (true) {
+		cout << "======================  欢迎来到传智播客机房预约系统  ====================="
+			<< endl;
+		cout << endl << "请输入您的身份" << endl;
+		cout << "\t\t -------------------------------\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          1.任课老师           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          2.审    核           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          3.管 理 员           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t|          0.退    出           |\n";
+		cout << "\t\t|                               |\n";
+		cout << "\t\t -------------------------------\n";
+		cout << "输入您的选择: ";
+
+		cin >> select;
+
+		switch (select)
+		{
+		case 1: // 任课老师接口
+			break;
+		case 2: //审核接口
+			break;
+		case 3: // 管理员接口
+			break;
+		case 0: // 退出
+			break;
+		default:
+			cout << "输入有误请重新输入" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
