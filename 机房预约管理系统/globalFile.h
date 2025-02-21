deb7605c4e0337384d664c6fa990593e1cb7f218
卜乐宇 <isbobo0701@gmail.com>
2025-02-21 21:46:18 +0800

    文件名的宏定义

---
+++ 机房预约管理系统/globalFile.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//管理员文件
+#define ADMIN_FILE "admin.txt"
+
+//老师文件
+#define TEACHER_FILE "teacher.txt"
+
+//审核文件
+#define AUDITOR_FILE "auditor.txt"
+
+//机房信息文件
+#define COMPUTER_FILE "computorroom.txt"
+
+//订单文件
+#define ORDER_FILE "order.txt"
+
