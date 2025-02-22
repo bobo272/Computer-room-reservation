acce10bdca8f48ffae8358bee549de181d51e49a
卜乐宇 <isbobo0701@gmail.com>
2025-02-22 14:23:40 +0800

    登录模块初实现

--- 机房预约管理系统/main.cpp
+++ 机房预约管理系统/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include "globalFile.h"
+#include "Identity.h"
+#include <string>
 
 using namespace std;
+
+void LoginIn(string fileName, int type);
+
 
 int main() {
 	int select = 0;
@@ -25,11 +32,14 @@ int main() {
 
 		switch (select)
 		{
-		case 1: // 任课老师接口
+		case 1://老师接口 
+			LoginIn(TEACHER_FILE, 1);
 			break;
 		case 2: //审核接口
+			LoginIn(AUDITOR_FILE, 2);
 			break;
 		case 3: // 管理员接口
+			LoginIn(ADMIN_FILE, 3);
 			break;
 		case 0: // 退出
 			cout << "欢迎下一次使用" << endl;
@@ -45,4 +55,59 @@ int main() {
 	}
 	system("pause");
 	return 0;
+}
+
+
+
+void LoginIn(string fileName, int type)
+{
+	Identity * person = nullptr;
+
+	ifstream ifs;
+	ifs.open(fileName, ios::in);
+
+	//假如fileNAme的文件不存在
+
+	if (!ifs.is_open())
+	{
+		cout << "出错，文件夹不存在" << endl;
+		ifs.close();
+		return;
+	}
+
+	//初始化各个数据
+	int id = 0;
+	string name;
+	string pwd;
+
+	if (type == 1||type == 2)
+	{
+		cout << "输入你的id" << endl;
+		cin >> id;
+	}
+
+	cout << "输入你的用户名" << endl;
+	cin >> name;
+
+	cout << "输入密码" << endl;
+	cin >> pwd;
+
+	if (type == 1)
+	{
+		//老师登录验证
+	}
+	else if (type == 2)
+	{
+		//审核登录验证
+	}
+	else if (type == 3)
+	{
+		//管理员登录验证
+	}
+
+	cout << "验证登录失败!" << endl;
+
+	system("pause");
+	system("cls");
+	return;
 }
